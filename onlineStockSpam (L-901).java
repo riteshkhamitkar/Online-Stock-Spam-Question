811e8f7a30cb71b1bd52ea35c056fa52dd2c3d93
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-11-09 09:10:07 +0530

    Create onlineStockSpam (L-901).java

---
+++ onlineStockSpam (L-901).java
@@ -0,0 +1,35 @@
+class StockSpanner {
+    
+    class Pair {
+        int price;
+        int index;
+        
+        Pair(Integer price , Integer index){
+            this.price = price;
+            this.index = index;
+        }
+    }
+    
+    Stack<Pair> st = new Stack<>(); ;
+    int index = -1;
+    
+    public StockSpanner() {
+        // st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        index++;
+        Pair pair = new Pair(price , index);
+        while(!st.isEmpty() && price >= st.peek().price) {
+				st.pop();
+			}
+			if(st.isEmpty()) {
+                st.push(pair);
+				return index + 1;
+			}else {
+				int prevHigh = st.peek().index;	
+                st.push(pair);
+				return index - prevHigh;
+			}
+    }
+}
